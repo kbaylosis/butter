063ac54ce6038e40fdbcd76f16771b030a372824
Kenneth Baylosis <kbaylosis@zoogtech.com>
2020-04-21 01:38:19 +0800

    Discontinue the use of onDataChanged

--- lib/base_ui_model.dart
+++ lib/base_ui_model.dart
@@ -1,13 +1,8 @@
-
 import 'package:flutter/material.dart';
 
 typedef OnDataChangedFunc = Function(Map<String, dynamic>);
 
 @immutable
 abstract class BaseUIModel {
-  final OnDataChangedFunc onDataChanged;
-
-  BaseUIModel({
-    this.onDataChanged,
-  });
+  
 }
