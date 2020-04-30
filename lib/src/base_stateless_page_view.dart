e0853f978acf9c03ed052053322b12c5a5a41fc0
Kenneth Baylosis <kbaylosis@zoogtech.com>
2020-04-30 23:34:45 +0800

    Define type of BaseStatefulPageView.specs

--- lib/src/base_stateless_page_view.dart
+++ lib/src/base_stateless_page_view.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/widgets.dart';
 
+import 'base_page_specs.dart';
 import 'base_page_view.dart';
 
+/// An implementation of the [BasePageView] using a [StatelessWidget]
+/// 
+/// Use this for lightweight pages not needing special handling of page cycles
 abstract class BaseStatelessPageView extends StatelessWidget
     implements BasePageView {
+
+  /// The page specs of type [BasePageSpecs]
   @override
-  get specs => null;
+  BasePageSpecs get specs => null;
 }
