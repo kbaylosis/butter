d0ef1d5ed983cfdbba50892d1bde5fea0c2b0a3e
Kenneth Baylosis <kbaylosis@zoogtech.com>
2020-04-30 11:58:29 +0800

    Add doc to BaseDispatcher

--- lib/src/base_dispatcher.dart
+++ lib/src/base_dispatcher.dart
@@ -1,6 +1,9 @@
 import 'base_ui_model.dart';
 
+/// A base interface for dispatching actions to the [Store]
 abstract class BaseDispatcher {
+
+  /// Sends an anonymous action given the data of a model of type [BaseUIModel]
   dispatchModel<Model extends BaseUIModel>(
       Model defaultModel, void Function(Model m) f,
       [bool overwrite = false]);
