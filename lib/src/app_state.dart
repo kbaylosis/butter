1b8d1c4fcf0b11a4f7ea31d5de294f18b0ae264b
Kenneth Baylosis <kbaylosis@zoogtech.com>
2020-04-30 10:23:58 +0800

    Provide doc for AppState

--- lib/src/app_state.dart
+++ lib/src/app_state.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 
+/// Stores the application's state
 class AppState {
   final Map<String, dynamic> _data;
 
@@ -7,6 +8,7 @@ class AppState {
     @required Map<String, dynamic> data,
   }) : _data = data;
 
+  /// Copies a value into the store
   AppState copy({String key, dynamic value}) {
     final Map<String, dynamic> newData = Map<String, dynamic>.from(this._data);
     newData[key] = value;
@@ -14,6 +16,7 @@ class AppState {
     return AppState(data: newData);
   }
 
+  /// Copies a set of values into the store
   AppState copyAll(Map<String, dynamic> map) {
     final Map<String, dynamic> newData = Map<String, dynamic>.from(this._data);
     newData.addAll(map);
@@ -21,6 +24,7 @@ class AppState {
     return AppState(data: newData);
   }
 
+  /// Copies multiple sets of values into the store
   AppState copyMultiple(List<Map<String, dynamic>> maps) {
     final Map<String, dynamic> newData = Map<String, dynamic>.from(this._data);
     maps.forEach((map) {
@@ -30,6 +34,7 @@ class AppState {
     return AppState(data: newData);
   }
 
+  /// Retrieves a value from the store
   T read<T>(String key, [T defValue]) {
     return this._data[key] ?? defValue;
   }
