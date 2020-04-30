3551965a541b241bae0cb4ae79ba597893370679
Kenneth Baylosis <kbaylosis@zoogtech.com>
2020-04-30 10:58:31 +0800

    Remove BaseDispatcher.dispatchAttribs()

--- lib/src/base_dispatcher.dart
+++ lib/src/base_dispatcher.dart
@@ -1,9 +1,6 @@
 import 'base_ui_model.dart';
 
 abstract class BaseDispatcher {
-  @deprecated
-  dispatchAttribs(Map<String, dynamic> data);
-
   dispatchModel<Model extends BaseUIModel>(
       Model defaultModel, void Function(Model m) f,
       [bool overwrite = false]);
