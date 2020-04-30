7e9015c54bb5501da5556c7e5dca7e9ae9ec47be
Kenneth Baylosis <kbaylosis@zoogtech.com>
2020-04-30 23:29:22 +0800

    Update the definition of beforeLoad() and beforeUpdate() of BaseStatefulPageView

--- lib/src/base_stateful_page_view.dart
+++ lib/src/base_stateful_page_view.dart
@@ -1,49 +1,85 @@
+import 'dart:async';
+
+import 'package:butter/src/base_page_specs.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 import 'base_page_view.dart';
 
+/// An implementation of the [BasePageView] using a [StatefulWidget]
+/// 
+/// It exposes methods that allow handling of a page when it is about to load or update,
+/// and when loading produces an error. This makes it possible to run asynchronous
+/// processes when a page loads or updates.
 abstract class BaseStatefulPageView extends StatefulWidget
     implements BasePageView {
   @override
   State<StatefulWidget> createState() => _BaseStatefulPageViewState();
 
-  beforeLoad() => true;
+  /// Called when a page is about to be loaded.
+  /// 
+  /// [buildLoading] is called while waiting for the result of this function.
+  /// 
+  /// Returns either a [Future<bool>] or [bool]. If [true] is returned, the page
+  /// will load normally, otherwise, it will not proceed to calling [build].
+  FutureOr<bool> beforeLoad() => true;
 
-  beforeUpdate() => true;
+  /// Called when a page is about to be updated.
+  /// 
+  /// Returns either a [Future<bool>] or [bool]. If [true] is returned, the page
+  /// will update normally, otherwise, it will not proceed to calling [build].
+  FutureOr<bool> beforeUpdate() => true;
 
+  /// Called while waiting for the result of [beforeLoad]
+  /// 
+  /// Returns the [Widget] to render on the page
   Widget buildLoading(BuildContext context) {
     return Scaffold();
   }
 
+  /// Renders the 'normal' view of the page
+  /// 
+  /// Returns an empty [Scaffold] by default.
   Widget build(BuildContext context) {
     return Scaffold();
   }
 
+  /// Renders the 'error' view of the page
+  /// 
+  /// Returns an empty [Scaffold] by default.
   Widget buildError(BuildContext context) {
     return Scaffold();
   }
 
+  /// The page specs of type [BasePageSpecs]
   @override
-  get specs => null;
+  BasePageSpecs get specs => null;
 }
 
+/// The [State] implementation of [BaseStatefulPageView]
+/// 
+/// It manages the page cycle to make it possible for loading and update events
+/// to be intercepted.
 class _BaseStatefulPageViewState extends State<BaseStatefulPageView> {
   bool initialized;
-  var loadRetVal;
+  bool loadRetVal;
 
+  /// Initializes [initialized] and [loadRetVal]
   @override
   void initState() {
     super.initState();
 
     initialized = false;
+    loadRetVal = false;
   }
 
+  /// Housekeeping of [_BaseStatefulPageViewState] 
   @override
   void dispose() {
     super.dispose();
   }
 
+  /// Builds the view of the page depending on its state (loading, updating or error)
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
@@ -60,9 +96,12 @@ class _BaseStatefulPageViewState extends State<BaseStatefulPageView> {
     );
   }
 
-  Future _cycle() {
+  /// Manages the page cycle
+  /// 
+  /// Returns null if page loading or updating needs to be aborted
+  Future<bool> _cycle() async {
     if (initialized) {
-      if (loadRetVal == null) {
+      if (!loadRetVal) {
         return null;
       }
 
