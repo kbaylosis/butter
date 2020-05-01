5b91dbd923f99b9876a638d1f722db2c912e06a8
Kenneth Baylosis <kbaylosis@zoogtech.com>
2020-05-01 17:28:27 +0800

    Replace all List.from with List.of

--- lib/src/app_state.dart
+++ lib/src/app_state.dart
@@ -10,7 +10,7 @@ class AppState {
 
   /// Copies a value into the store
   AppState copy({String key, dynamic value}) {
-    final Map<String, dynamic> newData = Map<String, dynamic>.from(this._data);
+    final Map<String, dynamic> newData = Map<String, dynamic>.of(this._data);
     newData[key] = value;
 
     return AppState(data: newData);
@@ -18,7 +18,7 @@ class AppState {
 
   /// Copies a set of values into the store
   AppState copyAll(Map<String, dynamic> map) {
-    final Map<String, dynamic> newData = Map<String, dynamic>.from(this._data);
+    final Map<String, dynamic> newData = Map<String, dynamic>.of(this._data);
     newData.addAll(map);
 
     return AppState(data: newData);
@@ -26,7 +26,7 @@ class AppState {
 
   /// Copies multiple sets of values into the store
   AppState copyMultiple(List<Map<String, dynamic>> maps) {
-    final Map<String, dynamic> newData = Map<String, dynamic>.from(this._data);
+    final Map<String, dynamic> newData = Map<String, dynamic>.of(this._data);
     maps.forEach((map) {
       newData.addAll(map);
     });
