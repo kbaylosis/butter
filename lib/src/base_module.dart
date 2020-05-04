1476ca795dafaa6f32791e4100c84457b3463f5e
Kenneth Baylosis <kbaylosis@zoogtech.com>
2020-05-04 21:51:41 +0800

    Handle navigator routing for submodules

--- lib/src/base_module.dart
+++ lib/src/base_module.dart
@@ -11,29 +11,66 @@ abstract class BaseModule extends StatelessWidget {
   final String routeName;
 
   /// The subroutes of this module
-  final Map<String, BasePageConnector<BasePageState, BasePageView>> routes;
+  final Map<String, Widget> routes;
+
+  /// Child modules
+  final Map<String, BaseModule> modules;
 
   /// Create a module by passing its [routeName] and all [routes] under it
   BaseModule({
     @required this.routeName,
     @required this.routes,
+    this.modules,
   });
 
   /// Renders this module
   @override
-  Widget build(BuildContext context) => getCurrentRoute(context);
+  Widget build(BuildContext context) => getRoute(context);
+
+  String getRouteName(BuildContext context) =>
+      NavigateAction.getCurrentNavigatorRouteName(context);
 
   /// Retrieves the current route
-  BasePageConnector<BasePageState, BasePageView> getCurrentRoute(
+  BasePageConnector<BasePageState, BasePageView> getRoute(
       BuildContext context) {
-    String routeName = NavigateAction.getCurrentNavigatorRouteName(context);
+    String routeName = this.getRouteName(context);
 
     if (routes.containsKey(routeName)) {
-      return routes[routeName];
-    } else if (routes.containsKey('/')) {
-      return routes['/'];
+      return _fetchRouteByType(context, routeName);
+    } else {
+      return _fetchRouteByType(context, '/');
+    }
+  }
+
+  /// Retrieves the current route
+  BasePageConnector<BasePageState, BasePageView> getChild(
+      BuildContext context) {
+    if (modules == null) {
+      return null;
     }
 
-    throw UnimplementedError();
+    String routeName = this.getRouteName(context);
+
+    if (modules.containsKey(routeName)) {
+      return modules[routeName].getRoute(context);
+    } else if (modules.containsKey('/')) {
+      return modules['/'].getRoute(context);
+    }
+
+    return this.getRoute(context);
+  }
+
+  BasePageConnector<BasePageState, BasePageView> _fetchRouteByType(
+      BuildContext context, String routeName) {
+    var r = routes[routeName];
+    if (r is BasePageConnector) {
+      return r;
+    } else if (r is BaseModule) {
+      return r.getRoute(context);
+    }
+
+    throw FormatException(
+        'Invalid type in BaseModule.routes for route [$routeName] in [${this.routeName}]. '
+        'Must of be of type BasePageConnector or BaseModule.');
   }
 }
