38e2c7a05101aa483d5609ecc1751a2eeeba837f
Kenneth Baylosis <kbaylosis@zoogtech.com>
2020-05-09 01:26:43 +0800

    Remove the Butter.production flag; Format and document butter/fx_log

--- lib/src/butter.dart
+++ lib/src/butter.dart
@@ -1,21 +1,41 @@
 import 'package:logger/logger.dart';
 
+/// Provider of first class butter library objects and settings
 class Butter {
-  static var production = false;
+  /// If true, the framework logs will be appear on the console
   static var showFxLogs = false;
+
+  /// If true, appends the timestamp on the logs
   static var showTimestamp = false;
+
+  /// The logger object
   static Logger get log => Logger(
         printer: _SimpleLogPrinter(),
       );
 
+  /// Sets the log level
+  static set level(LogLevel l) => Logger.level = l.alias;
+
+  /// Shorthand for the verbose log
   static get v => log.v;
+
+  /// Shorthand for the debug log
   static get d => log.d;
+
+  /// Shorthand for the info log
   static get i => log.i;
+
+  /// Shorthand for the warning log
   static get w => log.w;
+
+  /// Shorthand for the erro log
   static get e => log.e;
+
+  /// Shorthand for the fata log
   static get f => log.wtf;
 }
 
+/// Logger printer configuration
 class _SimpleLogPrinter extends LogPrinter {
   static final levelEmojis = {
     Level.verbose: '',
@@ -26,25 +46,62 @@ class _SimpleLogPrinter extends LogPrinter {
     Level.wtf: '👾',
   };
 
+  /// Handles the print formatting of the log
   @override
   List<String> log(LogEvent event) {
-    if (extractMessage(event.message)['fx'] && !Butter.showFxLogs) {
+    if (_extractMessage(event.message)['fx'] && !Butter.showFxLogs) {
       return [];
     }
 
     return [
-      '🧈: ${Butter.showTimestamp ? '${DateTime.now()}: ' : ''}${levelEmojis[event.level]} ${extractMessage(event.message)['m']}'
+      '🧈: ${Butter.showTimestamp ? '${DateTime.now()}: ' : ''}${levelEmojis[event.level]} ${_extractMessage(event.message)['m']}'
     ];
+  }
+
+  /// Formats a message object into a map
+  Map _extractMessage(var message) {
+    if (message is String) {
+      return {
+        'fx': false,
+        'm': message.toString(),
+      };
+    }
+
+    return message;
   }
 }
 
-Map extractMessage(var message) {
-  if (message is String) {
-    return {
-      'fx': false,
-      'm': message.toString(),
-    };
-  }
+/// Butter log levels
+enum LogLevel {
+  verbose,
+  debug,
+  info,
+  warning,
+  error,
+  fatal,
+  nothing,
+}
 
-  return message;
+/// An extension to LogLevel
+extension _LogLevelExtension on LogLevel {
+  /// Generates an alias of type Logger Level
+  Level get alias {
+    switch (this) {
+      case LogLevel.verbose:
+        return Level.verbose;
+      case LogLevel.info:
+        return Level.info;
+      case LogLevel.warning:
+        return Level.warning;
+      case LogLevel.error:
+        return Level.error;
+      case LogLevel.fatal:
+        return Level.wtf;
+      case LogLevel.nothing:
+        return Level.nothing;
+      case LogLevel.debug:
+      default:
+        return Level.debug;
+    }
+  }
 }
--- lib/src/fx_log.dart
+++ lib/src/fx_log.dart
@@ -1,31 +1,40 @@
 import 'butter.dart';
 
+/// Provides shorthand logging capability inside the butter library
+///
+/// This class is not exposed outside of the library
 class FxLog {
+  /// A shorthand for the verbose logging
   static void v(String message) => Butter.v({
         'm': message,
         'fx': true,
       });
 
+  /// A shorthand for the debug logging
   static void d(String message) => Butter.d({
         'm': message,
         'fx': true,
       });
 
+  /// A shorthand for the info logging
   static void i(String message) => Butter.i({
         'm': message,
         'fx': true,
       });
 
+  /// A shorthand for the warnign logging
   static void w(String message) => Butter.w({
         'm': message,
         'fx': true,
       });
 
+  /// A shorthand for the error logging
   static void e(String message) => Butter.e({
         'm': message,
         'fx': true,
       });
 
+  /// A shorthand for the fatal logging
   static void f(String message) => Butter.f({
         'm': message,
         'fx': true,
