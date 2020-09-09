2c66a94f63a89357f0791a0282099470aacf0b79
Kenneth Baylosis <kbaylosis@zoogtech.com>
2020-09-09 18:27:03 +0800

    Add dispatchFutureModel

--- lib/src/base_action.dart
+++ lib/src/base_action.dart
@@ -69,10 +69,25 @@ class BaseAction extends ReduxAction<AppState>
   /// across the whole app. Setting [overwrite] to true forces [dispatchModel] not to
   /// reuse any portion of the [Model] in the [Store].
   @override
-  dispatchModel<Model extends BaseUIModel>(
+  void dispatchModel<Model extends BaseUIModel>(
           Model defaultModel, void Function(Model m) f,
           [bool overwrite = false]) =>
       super.dispatch(BaseAction.build({
+        defaultModel.$key: mutate(defaultModel, f, overwrite),
+      }));
+
+  /// Sends an anonymous action given the data of a model of type [BaseUIModel]
+  ///
+  /// The key used for the model must be guaranteed as unique by the developer
+  /// across the whole app. Setting [overwrite] to true forces [dispatchModel] not to
+  /// reuse any portion of the [Model] in the [Store].
+  ///
+  /// Returns a [Future<void>]
+  @override
+  Future<void> dispatchFutureModel<Model extends BaseUIModel>(
+          Model defaultModel, void Function(Model m) f,
+          [bool overwrite = false]) =>
+      super.dispatchFuture(BaseAction.build({
         defaultModel.$key: mutate(defaultModel, f, overwrite),
       }));
 
--- lib/src/base_dispatcher.dart
+++ lib/src/base_dispatcher.dart
@@ -7,7 +7,18 @@ abstract class BaseDispatcher {
   /// The key used for the model must be guaranteed as unique by the developer
   /// across the whole app. Setting [overwrite] to true forces [dispatchModel] not to
   /// reuse any portion of the [Model] in the [Store].
-  dispatchModel<Model extends BaseUIModel>(
+  void dispatchModel<Model extends BaseUIModel>(
+      Model defaultModel, void Function(Model m) f,
+      [bool overwrite = false]);
+
+  /// Sends an anonymous action given the data of a model of type [BaseUIModel]
+  ///
+  /// The key used for the model must be guaranteed as unique by the developer
+  /// across the whole app. Setting [overwrite] to true forces [dispatchModel] not to
+  /// reuse any portion of the [Model] in the [Store].
+  ///
+  /// Returns a [Future<void>]
+  Future<void> dispatchFutureModel<Model extends BaseUIModel>(
       Model defaultModel, void Function(Model m) f,
       [bool overwrite = false]);
 }
